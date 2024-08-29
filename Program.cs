ac1f03eaeeed96f3f0bdf93cc56c1a826fab9cdb
KoWyNx <fberrani@hotmail.fr>
2024-08-29 12:14:20 +0200

    ajout du port dans le cors

--- Program.cs
+++ Program.cs
@@ -49,7 +49,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowSpecificOrigin",
         builder => builder
-            .WithOrigins("http://localhost:3000") 
+            .WithOrigins("http://localhost:5173") 
             .AllowAnyMethod()
             .AllowAnyHeader());
 });
